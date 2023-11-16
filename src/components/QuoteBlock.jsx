e35bde336d48060bc7cb6d99d5856b479af77dbf
Cheke <chekelosos@gmail.com>
2023-11-16 11:28:44 -0600

    Added functioanlity to change the quote and added animations

--- src/components/QuoteBlock.jsx
+++ src/components/QuoteBlock.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import './sass/QuoteBlock.sass'
+import { useUpdateTheme } from "./ThemeProvider";
 
 export default function Quoteblock(){
     const [quote, setQuote ] = useState('')
     const [author, setAuthor ] = useState('')
+
+    const updateTheme = useUpdateTheme()
 
     const getRandomQuote = async () => {
         await fetch('https://api.quotable.io/random').then(
@@ -17,16 +20,23 @@ export default function Quoteblock(){
         getRandomQuote()
     }, [])
 
+    const getNewQuoteAndUpdateTheme =  async () => {
+        await setQuote('')
+        await setAuthor('')
+        await getRandomQuote()
+        updateTheme()
+    }
+
     return (
         <div id="quote-box">
-            <p id="text"><i className="fa fa-quote-left"></i> {quote}</p>
-            <p id="author">{author}</p>
+            {quote ? <p id="text"><i className="fa fa-quote-left"></i> {quote}</p> : <p className="text"><i className="fa fa-quote-left"></i></p>}
+            {author ? <p id="author">{author}</p> : <></>}
             <div className="social-media-and-button-container">
                 <div className="social-media">
                     <a id="tweet-quote" className="social-media-button"><i className="fa-brands fa-twitter"></i></a>
                     <a id="tumblr-quote" className="social-media-button"><i className="fa-brands fa-tumblr"></i></a>
                 </div>
-                <button id="new-quote">New Quote</button>
+                <button id="new-quote" onClick={getNewQuoteAndUpdateTheme}>New Quote</button>
             </div>
         </div>
     )
