2714d7206634dd35e3fadb3362a7ebdd35d213c5
Cheke <chekelosos@gmail.com>
2023-11-16 13:05:19 -0600

    Added animations to the text

--- src/components/QuoteBlock.jsx
+++ src/components/QuoteBlock.jsx
@@ -33,8 +33,8 @@ export default function Quoteblock(){
             {author ? <p id="author">{author}</p> : <></>}
             <div className="social-media-and-button-container">
                 <div className="social-media">
-                    <a id="tweet-quote" className="social-media-button"><i className="fa-brands fa-twitter"></i></a>
-                    <a id="tumblr-quote" className="social-media-button"><i className="fa-brands fa-tumblr"></i></a>
+                    <a id="tweet-quote" href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.split(' ').join('%20')}. ${author.split(' ').join('%20')}}`} className="social-media-button"><i className="fa-brands fa-twitter"></i></a>
+                    <a href={`href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${author.split(' ').join('%20')}&content=${quote.split(' ').join('%20')}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"`}id="tumblr-quote" className="social-media-button"><i className="fa-brands fa-tumblr"></i></a>
                 </div>
                 <button id="new-quote" onClick={getNewQuoteAndUpdateTheme}>New Quote</button>
             </div>
